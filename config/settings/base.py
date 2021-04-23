65c93f8ae51730b845ca45d67fe8eb33dc76a28f
justinjohnymathew <justinjohny.jj@gmail.com>
2021-04-23 12:33:54 +0530

    changed Base

--- config/settings/base.py
+++ config/settings/base.py
@@ -52,7 +52,7 @@ DATABASES["default"]["ATOMIC_REQUESTS"] = True
 ROOT_URLCONF = "config.urls"
 # https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
 WSGI_APPLICATION = "config.wsgi.application"
-ASGI_APPLICATION = "config.asgi.application"
+# ASGI_APPLICATION = "config.asgi.application"
 
 # APPS
 # ------------------------------------------------------------------------------
