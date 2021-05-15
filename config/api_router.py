031a8d37a28e31af04f72e987963162f5bbbaabd
justinjohnymathew <justinjohny.jj@gmail.com>
2021-05-15 23:34:02 +0530

    addded enrollment models

--- config/api_router.py
+++ config/api_router.py
@@ -1,7 +1,13 @@
 from django.conf import settings
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter, SimpleRouter
+from rest_framework_nested import routers
 
-from backend_api.clasroom.api.views import ClassRoomViewSet
+from backend_api.clasroom.api.views import (
+    ClassRoomViewSet,
+    EnrollViewSet,
+    StudentViewset,
+)
 
 # from backend_api.users.api.views import UserViewSet
 
@@ -12,7 +18,14 @@ else:
 
 # router.register("users", UserViewSet)
 router.register("clasroom", ClassRoomViewSet)
-
+router.register("students", StudentViewset)
+students_router = routers.NestedSimpleRouter(router, "clasroom", lookup="clasroom")
+students_router.register(
+    "enrolled-students", EnrollViewSet, basename="clasroom-students"
+)
 
 app_name = "api"
-urlpatterns = router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+    path("", include(students_router.urls)),
+]
