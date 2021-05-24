53317834451626b72411c0afd070d6ac4a98f924
justinjohnymathew <justinjohny.jj@gmail.com>
2021-05-24 22:32:45 +0530

    added modules and notes serializer to api router

--- config/api_router.py
+++ config/api_router.py
@@ -1,11 +1,15 @@
 from django.conf import settings
+
+# from django.db.models import base
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter, SimpleRouter
 from rest_framework_nested import routers
 
-from backend_api.clasroom.api.views import (  # ModuleFileVieset,
+from backend_api.clasroom.api.views import (
     ClassRoomViewSet,
     EnrollViewSet,
+    ModuleFileVieset,
+    ModuleViewSet,
     MyCourseViewset,
     StudentViewset,
 )
@@ -26,9 +30,12 @@ students_router = routers.NestedSimpleRouter(router, "clasroom", lookup="clasroo
 students_router.register(
     "enrolled-students", EnrollViewSet, basename="clasroom-students"
 )
-
+students_router.register("modules", ModuleViewSet, basename="module")
+modules_router = routers.NestedSimpleRouter(students_router, "modules", lookup="module")
+modules_router.register("notes", ModuleFileVieset, basename="notes")
 app_name = "api"
 urlpatterns = [
     path("", include(router.urls)),
     path("", include(students_router.urls)),
+    path("", include(modules_router.urls)),
 ]
